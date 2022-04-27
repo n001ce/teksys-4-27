f557ddea7f2582d4ffd981777cccc4c6cd33a71b
Alex <a1ccormier@gmail.com>
2022-04-27 11:03:12 -0400

    second push

---
+++ src/filie/io/FileClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package filie.io;
+
+/**
+ * @author Kepler01635
+ *
+ */
+public class FileClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
