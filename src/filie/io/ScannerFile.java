f609329b1046f4943d21d28540b21b276a2edd8b
Alex <a1ccormier@gmail.com>
2022-04-27 11:16:40 -0400

    fifth push

--- src/filie/io/ScannerFile.java
+++ src/filie/io/ScannerFile.java
@@ -1,12 +1,24 @@
 package filie.io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ScannerFile {
 	public static void main(String args[]) {
-		String location = "file location";
-		File file = new File(location);
-		Scanner input = new Scanner(file);
-		String line = input.nextLine();
+		//Account for any errors that might occur
+		try {
+			String location = "file location";
+			File file = new File(location);
+			Scanner input = new Scanner(file);
+			String line = input.nextLine();
+		}catch(FileNotFoundException e){
+			System.out.println("File not found!");
+			e.printStackTrace();
+		}
+//		String location = "file location";
+//		File file = new File(location);
+//		Scanner input = new Scanner(file);
+//		String line = input.nextLine();
 	}
 }
