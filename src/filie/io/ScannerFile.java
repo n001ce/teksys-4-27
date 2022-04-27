83e3404f0e8461f68a61e540590b5bc27bd60c15
Alex <a1ccormier@gmail.com>
2022-04-27 11:20:36 -0400

    sixth push

--- src/filie/io/ScannerFile.java
+++ src/filie/io/ScannerFile.java
@@ -6,16 +6,32 @@ import java.util.Scanner;
 
 public class ScannerFile {
 	public static void main(String args[]) {
-		//Account for any errors that might occur
+		//Read an entire file
 		try {
-			String location = "file location";
+			String location = "location path";
 			File file = new File(location);
 			Scanner input = new Scanner(file);
-			String line = input.nextLine();
-		}catch(FileNotFoundException e){
+			String data = "";
+			while(input.hasNextLine()) { //while file has next line, data becomes that line
+				data = input.nextLine();
+			}
+		}catch(FileNotFoundException e) { //if file isn't found print file not found
 			System.out.println("File not found!");
 			e.printStackTrace();
 		}
+		
+		
+		//Account for any errors that might occur
+//		try {
+//			String location = "file location";
+//			File file = new File(location);
+//			Scanner input = new Scanner(file);
+//			String line = input.nextLine();
+//		}catch(FileNotFoundException e){
+//			System.out.println("File not found!");
+//			e.printStackTrace();
+//		}
+		//Read a files first line.
 //		String location = "file location";
 //		File file = new File(location);
 //		Scanner input = new Scanner(file);
